9f6920d45d2c5904564e83f1cfce770cb878138f
Finder <44934843+finder@users.noreply.github.com>
2020-06-02 13:02:50 -0600

    switch from local file to deno.land

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-import njalla from './mod.js' // import the njalla module
+import njalla from 'https://deno.land/x/njalla/mod.js' // import the njalla module
 
 console.log(await njalla('1337')) // returns an array of available domains
